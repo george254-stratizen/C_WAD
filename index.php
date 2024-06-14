d8844b27ce724b056a5f81dcbe8f1eaff289499a
george254-stratizen <george.makau@strathmore.edu>
2024-06-14 09:57:21 +0300

    renamed all my files as php

--- index.php
+++ index.php
@@ -7,14 +7,7 @@
     </head>
     <body >
       
-      <a href="about.html  " > about   </a>
-      <a href="index.html  " > home   </a>
-      <a href="Fee.html  " > Fee   </a>
-      <a href="registration.html  " > Registration  </a>
-      <a href="campusLife.html  " > CampusLife   </a>
-      <a href="contact_us.html  " > Contact   </a>
-      <a href="courses.html" > Courses  </a><br><br>
-      <a href="signup.html">Sign Up</a>
+     <?php include_once("template/nav.php");?>
       <i class="fa fa-home" aria-hidden="true"></i>
        <h1>Welcome</h1>
       
