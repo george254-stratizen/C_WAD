8141f52c79ba17971893eb16657a01cf33829b34
george254-stratizen <george.makau@strathmore.edu>
2024-06-28 17:22:57 +0300

    Inserted default data into my table

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 28, 2024 at 03:10 PM
+-- Generation Time: Jun 28, 2024 at 04:20 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -27,85 +27,130 @@ USE `e_uni`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `gender`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `userid` bigint(11) NOT NULL AUTO_INCREMENT,
-  `fullname` varchar(50) NOT NULL DEFAULT '',
-  `username` varchar(50) NOT NULL DEFAULT '',
-  `email` varchar(50) NOT NULL DEFAULT '',
-  `password` varchar(50) NOT NULL DEFAULT '',
-  `user_roleId` int(1) NOT NULL DEFAULT 0,
+DROP TABLE IF EXISTS `gender`;
+CREATE TABLE `gender` (
+  `genderId` int(1) NOT NULL,
+  `gender` varchar(20) NOT NULL DEFAULT '',
   `datecreated` datetime DEFAULT current_timestamp(),
-  `dateupdated` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`userid`),
-  UNIQUE KEY `username` (`username`)
+  `dateupdated` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Truncate table before insert `users`
+-- Truncate table before insert `gender`
 --
 
-TRUNCATE TABLE `users`;COMMIT;
+TRUNCATE TABLE `gender`;
+--
+-- Dumping data for table `gender`
+--
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `gender` (`genderId`, `gender`, `datecreated`, `dateupdated`) VALUES
+(1, 'male', '2024-06-28 17:15:33', '2024-06-28 17:15:33'),
+(2, 'female', '2024-06-28 17:15:33', '2024-06-28 17:15:33'),
+(3, 'rather not say', '2024-06-28 17:15:33', '2024-06-28 17:15:33');
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- --------------------------------------------------------
 
 --
--- Database: `e_uni`
+-- Table structure for table `roles`
 --
-DROP DATABASE IF EXISTS `e_uni`;
-CREATE DATABASE IF NOT EXISTS `e_uni` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `e_uni`;
+
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE `roles` (
+  `roleId` int(1) NOT NULL,
+  `role` varchar(20) NOT NULL DEFAULT '',
+  `datecreated` datetime DEFAULT current_timestamp(),
+  `dateupdated` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Truncate table before insert `roles`
+--
+
+TRUNCATE TABLE `roles`;
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`roleId`, `role`, `datecreated`, `dateupdated`) VALUES
+(1, 'admin', '2024-06-28 17:13:22', '2024-06-28 17:13:22'),
+(2, 'student', '2024-06-28 17:13:22', '2024-06-28 17:13:22'),
+(3, 'instructor', '2024-06-28 17:13:22', '2024-06-28 17:13:22');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
+
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `userid` bigint(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `userid` bigint(11) NOT NULL,
   `fullname` varchar(50) NOT NULL DEFAULT '',
   `username` varchar(50) NOT NULL DEFAULT '',
   `email` varchar(50) NOT NULL DEFAULT '',
   `password` varchar(50) NOT NULL DEFAULT '',
   `user_roleId` int(1) NOT NULL DEFAULT 0,
   `datecreated` datetime DEFAULT current_timestamp(),
-  `dateupdated` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`userid`),
-  UNIQUE KEY `username` (`username`)
+  `dateupdated` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-DROP TABLE IF EXISTS `roles`;
-CREATE TABLE IF NOT EXISTS `roles` (
-  `roleId` int(1) NOT NULL AUTO_INCREMENT,
-  `role` varchar(20) NOT NULL DEFAULT '',
-  `datecreated` datetime DEFAULT current_timestamp(),
-  `dateupdated` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`roleId`),
-  UNIQUE KEY `role` (`role`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+--
+-- Truncate table before insert `users`
+--
 
-DROP TABLE IF EXISTS `gender`;
-CREATE TABLE IF NOT EXISTS `gender` (
-  `genderId` int(1) NOT NULL AUTO_INCREMENT,
-  `gender` varchar(20) NOT NULL DEFAULT '',
-  `datecreated` datetime DEFAULT current_timestamp(),
-  `dateupdated` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`genderId`),
-  UNIQUE KEY `gender` (`gender`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+TRUNCATE TABLE `users`;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `gender`
+--
+ALTER TABLE `gender`
+  ADD PRIMARY KEY (`genderId`),
+  ADD UNIQUE KEY `gender` (`gender`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`roleId`),
+  ADD UNIQUE KEY `role` (`role`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userid`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `gender`
+--
+ALTER TABLE `gender`
+  MODIFY `genderId` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `roleId` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userid` bigint(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
