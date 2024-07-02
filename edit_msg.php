38da1754073a52d64f10f66664c6956db4c53e11
george254-stratizen <george.makau@strathmore.edu>
2024-07-02 08:18:11 +0300

    added the rest of the data

--- edit_msg.php
+++ edit_msg.php
@@ -26,3 +26,36 @@
         }
     }
 ?>
+
+
+<div class="header">
+    <h1>Update Message</h1>
+</div>
+        
+<div class="row">
+    <div class="content">
+    <h1>Update Message</h1>
+    <form action="<?php print htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" class="contact_form">
+        <label for="Fn">Fullname:</label><br>
+        <input type="text" name="fullname" id="Fn" placeholder="Fullname" required value="<?php print $spot_msg_row["sender_name"]; ?>"><br><br>
+
+        <label for="email">Email Address:</label><br>
+        <input type="email" id="email" name="email_address" placeholder="Email address" required value="<?php print $spot_msg_row["sender_email"]; ?>"><br><br>
+        
+        <label for="sb">Subject:</label><br>
+        <select name="subject_line" id="sb" required>
+            <option value="<?php print $spot_msg_row["subject_line"]; ?>"><?php print $spot_msg_row["subject_line"]; ?></option>
+            <option value="Email Support">Email Support</option>
+            <option value="eLearning Support">eLearning Support</option>
+            <option value="AMS Support">AMS Support</option>
+        </select>
+        <br><br>
+    <label for="ms">Message:</label><br>
+    <textarea cols="30" rows="7" name="message" id="ms" required><?php print $spot_msg_row["message"]; ?></textarea><br><br>
+    <input type="submit" name="update_message" value="Update Message" >
+    <input type="hidden" name="messageId" value="<?php print $spot_msg_row["messageId"]; ?>" >
+</form>
+</div>
+
+        </div>      
+<?php include_once("template/footer.php");?>
