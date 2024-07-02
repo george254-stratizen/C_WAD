26450b33b32d8185d86dbd3cb269763944975831
george254-stratizen <george.makau@strathmore.edu>
2024-07-02 08:15:14 +0300

    error handling

--- edit_msg.php
+++ edit_msg.php
@@ -8,3 +8,21 @@
     $spot_msg = "SELECT * FROM `messages` WHERE messageId = '$messageId' LIMIT 1";
     $spot_msg_res = $conn->query($spot_msg);
     $spot_msg_row = $spot_msg_res->fetch_assoc();
+
+    if(isset($_POST["update_message"])){
+        $fullname = mysqli_real_escape_string($conn, $_POST["fullname"]);
+        $email = mysqli_real_escape_string($conn, $_POST["email_address"]);
+        $subject_line = mysqli_real_escape_string($conn, $_POST["subject_line"]);
+        $text_message = mysqli_real_escape_string($conn, $_POST["message"]);
+        $messageId = mysqli_real_escape_string($conn, $_POST["messageId"]);
+
+        $update_message = "UPDATE messages SET sender_name = '$fullname', sender_email = '$email', subject_line = '$subject_line', message = '$text_message' WHERE messageId='$messageId' LIMIT 1";
+      
+        if ($conn->query($update_message) === TRUE) {
+            header("Location: view_messages.php");
+            exit();
+        } else {
+            echo "Error: " . $update_message . "<br>" . $conn->error;
+        }
+    }
+?>
